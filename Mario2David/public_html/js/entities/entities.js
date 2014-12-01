c11ac18dec762d5efddd3efc2cd3a1af7ca76e8d
david perry <perryd11016@gmail.com>
2014-12-01 00:51:22 -0800

    video 14 done

--- Mario2David/public_html/js/entities/entities.js
+++ Mario2David/public_html/js/entities/entities.js
@@ -19,6 +19,7 @@ game.PlayerEntity = me.Entity.extend({
           is.renderable.setCurrentAnimation("idle");
  
           this.body.setVelocity(5, 20);
+          me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
       },
       
     update: function(delta){
@@ -63,6 +64,7 @@ game.PlayerEntity = me.Entity.extend({
     OnCollision: function(){
         this.body.setCollisionMask(me.collision.types.NO_OBJECT);
         me.levelDirector.loadLevel(this.level);
+        me.state.current().resetPlayer();
     }
     
 });
--- Mario2David/public_html/js/screens/play.js
+++ Mario2David/public_html/js/screens/play.js
@@ -8,7 +8,7 @@ game.PlayScreen = me.ScreenObject.extend({
 
                 me.levelDirector.loadLevel("Davidlv1");
 
-		this.resetPlayer();
+		this.resetPlayer(0, 400);
                 
                 me.input.bindKey(me.input.Key.RIGHT, "right");
             // add our HUD to the game world
@@ -27,8 +27,8 @@ game.PlayScreen = me.ScreenObject.extend({
 		me.game.world.removeChild(this.HUD);
 	},
         
-        resetPlayer: function(){
-            var player = me.pool.pull("mario", 0, 420, {});
+        resetPlayer: function(x, y){
+            var player = me.pool.pull("mario", x, y, {});
             me.game.world.addChild(player, 3);
         }
 });
